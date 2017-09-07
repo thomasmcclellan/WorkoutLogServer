e376230547ba4158a6aa1740da9e21cc43289dd4
Thomas McClellan <thomasmcclellan87@gmail.com>
2017-09-07 09:51:41 -0400

    Initial server commit

---
+++ app.js
@@ -0,0 +1,64 @@
+// This uses Nodemon
+// It is a utility that will monitor for any changes in your source and automatically restart your server
+// Used instead of node to allow that auto restart
+
+// npm install -g nodemon (then)
+// nodemon app.js
+
+require('dotenv').config();
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var sequelize = require('./db.js');
+
+var User = sequelize.import('./models/user');
+
+sequelize.sync();
+// sequelize.sync({ force: true }); //DANGER: THIS WILL DROP (DELETE) THE USER TABLE!!!
+
+// User.sync();
+// User.sync({ force: true }); //DANGER: THIS WILL DROP (DELETE) THE USER TABLE!!!
+
+app.use(bodyParser.json());
+
+// Middleware => CORS (Cross-Origin Resource Sharing)
+app.use(require('./middleware/headers'));
+app.use(require('./middleware/validate-session'));
+
+app.use('/api/user', require('./routes/user'));
+//login route
+app.use('/api/login', require('./routes/session'));
+app.use('/api/definition', require('./routes/definition'));
+app.use('/api/log', require('./routes/log'));
+
+//this links with client
+app.use('/api/test', function(req, res){
+	res.send('Hello World');
+});
+
+app.listen(3000, function(){
+	console.log("app is listening on 3000");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ routes/log.js
@@ -0,0 +1,107 @@
+var router = require('express').Router();
+var sequelize = require('../db');
+var Log = sequelize.import('../models/log');
+var User = sequelize.import('../models/user');
+var Definition = sequelize.import('../models/definition');
+
+router.post('/', function(req, res){
+	//req has some body properties that have a username and pwd
+	var description = req.body.log.description;
+	var result = req.body.log.result;
+	var user = req.user;
+	var definition = req.body.log.def;
+
+	//use our sequelize model to create log 
+	Log
+		.create({
+			description: description,
+			result: result,
+			owner: user.id,
+			def: definition
+		})
+		.then(
+			function createSuccess(log){
+				res.json(log);
+			},
+			function createError(err){
+				res.send(500, err.message);
+			}
+		);
+});
+
+router.get('/', function(req, res){
+	var userid = req.user.id;
+	Log
+		.findAll({
+			where: { owner: userid }
+		})
+		.then(
+			function findAllSuccess(data){
+				//console.log(data);
+				res.json(data);
+			},
+			function findAllError(err){
+				res.send(500, err.message);
+			}
+		);
+});
+
+//This will retrieve one workout specified by the log id
+router.get('/:id', function(req, res){
+	var data = req.params.id;
+	//console.log(data); 
+	Log
+		.findOne({
+			where: { id: data }
+		}).then(
+			function getSuccess(updateData){
+				res.json(updateData);
+			},
+			function getError(err){
+				res.send(500, err.message);
+			}
+		);
+});
+
+//This will return the data from the log that was updated
+router.put('/', function(req, res){
+	var description = req.body.log.desc;
+	var result = req.body.log.result;
+	var data = req.body.log.id;
+	var definition = req.body.log.def;
+	console.log(req);
+	Log
+		.update(
+		{
+			description: description,
+			result: result,
+			def: definition
+		},
+
+		{ where: { id: data } }
+		).then(
+			function updateSuccess(updateLog){
+				res.json(updateLog);
+			},
+			function updateError(err){
+				res.send(500, err.message);
+			}
+		)
+});
+
+router.delete("/", function(req, res){
+	var data = req.body.log.id;
+	Log
+		.destroy({
+			where: { id: data }
+		}).then(
+			function deleteLogSuccess(data){
+				res.send("you removed a log");
+			},
+			function deleteLogError(err){
+				res.send(500, err.message);
+			}
+		);
+});
+
+module.exports = router;
