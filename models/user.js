2c3d88b7e2d71aea08b9aa5fcbfe6bd1af3b3424
Thomas McClellan <thomasmcclellan87@gmail.com>
2017-09-13 15:08:22 -0400

    Update

--- models/user.js
+++ models/user.js
@@ -2,7 +2,6 @@ module.exports = function(sequelize, DataTypes){
 	return sequelize.define('user', {
 		username: DataTypes.STRING,
 		passwordhash: DataTypes.STRING,
-
 		firstName: DataTypes.STRING,
 		lastName: DataTypes.STRING,
 		age: DataTypes.INTEGER,
